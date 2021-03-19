26844e1015a74a06bd8fd33b3f1b6685b7d69f09
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-03-19 00:40:05 -0700

    add: isAccessAllowed, product schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,40 +1,68 @@
+import { createAuth } from '@keystone-next/auth'
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 import { config, createSchema } from '@keystone-next/keystone/schema'
 import 'dotenv/config'
+// import { permissionsList } from './schemas/fields'
 import { User } from './schemas/User'
+import { Product } from './schemas/Product'
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-my-online-shop'
-
-console.log({ databaseURL })
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET
 }
 
-export default config({
-  // @ts-ignore
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true
-    }
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    async onConnect(keystone) {
-      console.log('Connected to the database!')
-      // if (process.argv.includes('--seed-data')) {
-      //   await insertSeedData(keystone);
-      // }
-    }
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User
-  }),
-  ui: {
-    // Show the UI only for poeple who pass this test
-    isAccessAllowed: () => true
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password']
+    // TODO: Add in inital roles here
   }
+  // passwordResetLink: {
+  //   async sendToken(args) {
+  //     // send the email
+  //     await sendPasswordResetEmail(args.token, args.identity);
+  //   },
+  // },
 })
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true
+      }
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to the database!')
+        // if (process.argv.includes('--seed-data')) {
+        //   await insertSeedData(keystone);
+        // }
+      }
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product
+    }),
+    ui: {
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log({ session })
+        return !!session?.data
+      }
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      // User: `id name email role { ${permissionsList.join(' ')} }`
+    })
+  })
+)
